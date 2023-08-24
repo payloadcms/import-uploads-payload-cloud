79aac1a5bb2247b046d2b4ee79f02b2e29dce5b7
Elliot DeNolf <denolfe@gmail.com>
2023-08-24 14:50:35 -0400

    feat: button up all prompts to make friendlier

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Payload Cloud Upload Import
 
-Migrate existing uploads from a static directory into Payload Cloud's storage. NOTE: **The records must already exist in the database**.
+Migrate existing uploads from a static directory into Payload Cloud's storage. NOTE: **The records must already exist in the database. This is for moving files from local to Payload Cloud S3 storage**.
 
 ## Usage
 
-1. Copy .env.example to .env and fill out the values
-2. Run `yarn import-uploads`. This will dry-run by default
-3. After verifying, run `yarn import-uploads:prod`
+1. Copy .env.example to .env and fill out the values by visiting your cloud project's `File Storage` tab.
+2. Run `yarn import-uploads`. This will dry-run by default. Follow prompts.
+3. After verifying every _would run properly_, run `yarn import-uploads:prod` with the same values
--- src/import-uploads.ts
+++ src/import-uploads.ts
@@ -1,34 +1,57 @@
 import dotenv from 'dotenv'
 import path from 'path'
 import fs from 'fs'
+import minimist from 'minimist'
 import { getStorageClient, createKey } from '@payloadcms/plugin-cloud'
 import { fromBuffer } from 'file-type'
+import { input, confirm } from '@inquirer/prompts'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 })
 
-const { DRY_RUN } = parseArgs()
+const args = minimist(process.argv.slice(2))
+const DRY_RUN = args['dry-run'] === 'false' ? false : true
 console.log(`DRY_RUN: ${DRY_RUN}`)
 
-// Ensure this matches your payload config
-const collectionSlug = 'media'
-
-// Ensure this matches your import directory
-const importMediaDir = path.resolve(__dirname, '../import')
-
 const main = async () => {
+  const uploadDir = await input({ message: 'Enter absolute path of uploads directory' })
+  if (!uploadDir) {
+    console.log('No uploadDir provided')
+    return
+  }
+
+  const uploadSlug = await input({ message: 'Enter upload slug. Reference your Payload config' })
+  if (!uploadSlug) {
+    console.log('No uploadSlug provided')
+    return
+  }
+
+  const importUploadDir = path.resolve(uploadDir)
+
   validateAllEnvs()
 
-  // Get all files from the media directory
-  const files = fs.readdirSync(importMediaDir)
-  console.log(`Preparing to upload ${files.length} media files`)
+  // Get all files from the upload directory
+  const files = fs.readdirSync(importUploadDir)
+
+  // prompt to continue
+  const confirmContinue = await confirm({
+    message: `Preparing to upload ${files.length} files from ${importUploadDir}... Continue?`,
+    default: false,
+  })
+
+  if (!confirmContinue) {
+    console.log('Aborting upload operation.')
+    process.exit(0)
+  }
+
   const { storageClient, identityID } = await getStorageClient()
 
+  // TODO: Chunk this out into batches
   try {
     await Promise.all(
       files.map(async fileName => {
-        const logPrefix = `${DRY_RUN ? 'DRY RUN' : ''} ${fileName}:`
+        const logPrefix = `${DRY_RUN ? '[DRY RUN]' : ''} ${fileName}:`
         try {
           console.log(`${logPrefix} Uploading...`)
           if (DRY_RUN) {
@@ -36,19 +59,19 @@ const main = async () => {
             return
           } else {
             // get buffer of the file
-            const fullFilePath = path.resolve(importMediaDir, fileName)
+            const fullFilePath = path.resolve(importUploadDir, fileName)
             const fileBuffer = fs.readFileSync(fullFilePath)
             const mimeType = (await fromBuffer(fileBuffer)).mime
             await storageClient.putObject({
               Bucket: process.env.PAYLOAD_CLOUD_BUCKET,
-              Key: createKey({ collection: collectionSlug, filename: fileName, identityID }),
+              Key: createKey({ collection: uploadSlug, filename: fileName, identityID }),
               Body: fileBuffer,
               ContentType: mimeType,
             })
             console.log(`${logPrefix} Uploaded`)
           }
         } catch (err) {
-          console.error(`Media ${fileName} failed to create`)
+          console.error(`File ${fileName} failed to create`)
           console.error(err)
         } finally {
           console.log(`${logPrefix} Upload complete!`)
@@ -56,12 +79,12 @@ const main = async () => {
       }),
     )
   } catch (err) {
-    console.log('Unable to upload media')
+    console.log('Unable to upload file')
     console.error(err)
     process.exit(0)
   }
 
-  console.log('Media upload completed!')
+  console.log('Upload completed!')
   process.exit(0)
 }
 
